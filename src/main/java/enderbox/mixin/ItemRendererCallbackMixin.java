ae51ddd5f009522fbe3f6c9c81f4277e78997c85
Julian Dunskus <julian.dunskus@gmail.com>
2020-02-21 02:11:09 +0100

    better naming

--- src/main/java/enderbox/mixin/ItemRendererCallbackMixin.java
+++ src/main/java/enderbox/mixin/ItemRendererCallbackMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BuiltinModelItemRenderer.class)
-public abstract class BuiltinModelItemRendererMixin {
+public abstract class ItemRendererCallbackMixin {
 	@Inject(at = @At("HEAD"), method = "render", cancellable = true)
 	private void render(
 		ItemStack itemStack,
